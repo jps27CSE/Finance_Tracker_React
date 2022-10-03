09f7336777c779f6ae36ffc88f266c861af3929c
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-10-03 12:05:48 +0600

    install firebase and firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ50sO5Eu4cT5AjsZcEbEkjoKjmJFgnPo",
+  authDomain: "finance-tracker-react-57036.firebaseapp.com",
+  projectId: "finance-tracker-react-57036",
+  storageBucket: "finance-tracker-react-57036.appspot.com",
+  messagingSenderId: "901363114040",
+  appId: "1:901363114040:web:509335b8ee6e633ed7c6cd",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
