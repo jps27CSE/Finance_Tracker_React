6f25ff4f199c189d40f152eb400f4fc3d553b79b
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-10-06 13:02:36 +0600

    custom hook for signup

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const signup = async (email, password, display) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      //signup user
+      const responese = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(responese.user);
+
+      if (!responese) {
+        throw new Error("Could not Complete Signup");
+      }
+
+      //add display name to user
+
+      await responese.user.updateProfile({ displayName: displayName });
+
+      setIsPending(false);
+      setError(null);
+    } catch (error) {
+      console.error(error.message);
+      setError(error.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, signup };
+};
