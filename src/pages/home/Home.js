8c9184048678bef158bddd73bff53875493dc4ad
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-10-28 23:24:38 +0600

    Transaction Form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,13 @@
 import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
