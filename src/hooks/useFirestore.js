e8a7a093cf691932cdd3142e429bddb8df98c90b
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-11-10 13:33:25 +0600

    deleting transactions

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -19,6 +19,13 @@ const firestoreReducer = (state, action) => {
         success: true,
         error: null,
       };
+    case "DELETED_DOCUMENT":
+      return {
+        isPending: false,
+        document: null,
+        success: true,
+        error: null,
+      };
     case "ERROR":
       return {
         isPending: false,
@@ -61,7 +68,16 @@ export const useFirestore = (collection) => {
   };
 
   //delete document
-  const deleteDocument = async (id) => {};
+  const deleteDocument = async (id) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      await ref.doc(id).delete();
+      dispatchIfNotCancelled({ type: "DELETED_DOCUMENTS" });
+    } catch (error) {
+      dispatch({ type: "ERROR", payload: "Could not Delete" });
+    }
+  };
 
   useEffect(() => {
     return () => setIsCancelled(true);
